17cc40c63c9107fbf96a86d2df551a85cfab8388
Geysa Ribeiro <geysa.ribeirotoppan@gmail.com>
2025-04-29 20:41:38 -0300

    Alterações em dashboard e PostView

--- src/pages/Dashboard/indexDashboard.jsx
+++ src/pages/Dashboard/indexDashboard.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
 import styles from "./Dashboard.module.css";
 
@@ -6,9 +7,10 @@ function Dashboard() {
 
     useEffect(() => {
         const mockPosts = [
-            { id: 1, title: "Primeira postagem", content: "Conteúdo da primeira postagem" },
-            { id: 2, title: "Segunda postagem", content: "Conteúdo da segunda postagem" },
-            { id: 3, title: "Terceira postagem", content: "Conteúdo da terceira postagem" },
+            { id: 1, title: "Primeira postagem", content: "Conteúdo da primeira postagem", author:"Geysa" },
+            { id: 2, title: "Segunda postagem", content: "Conteúdo da segunda postagem", author:"João" },
+            { id: 3, title: "Terceira postagem", content: "Conteúdo da terceira postagem", author:"Maria" },
+            { id: 4, title: "Quarta postagem", content: "Conteúdo da quarta postagem", author:"Carlos" }
         ];
         setPosts(mockPosts);
     }, []);
@@ -16,15 +18,18 @@ function Dashboard() {
     return (
         <div className="{styles.dashboard">
             <h1>Painel de Postagens</h1>
-
             {posts.length === 0 ? (
                 <p>Nenhum post encontrado.</p>
             ) : (
                 <ul className={styles.postList}>
                     {posts.map((post) => (
                         <li key={post.id} className={styles.postItem}>
+                            <Link to={`/posts/${post.id}`}>
                             <h3>{post.title}</h3>
+                            </Link>
                             <p>{post.content}</p>
+                            <p><strong>Autor:</strong> {post.author}</p>
+    
                         </li>
                     ))}
                 </ul>
--- src/pages/PostView/index.jsx
+++ src/pages/PostView/index.jsx
@@ -3,9 +3,22 @@ import { useParams } from "react-router-dom";
 function PostView() {
     const { id } = useParams();
 
+    const mockPosts = [
+        { id: 1, title: "Primeira postagem", content: "Conteúdo da primeira postagem" },
+        { id: 2, title: "Segunda postagem", content: "Conteúdo da segunda postagem" },
+        { id: 3, title: "Terceira postagem", content: "Conteúdo da terceira postagem" },
+    ];
+
+    const post = mockPosts.find(p => post.id === parseInt(id));
+
+    if (!post) {
+        return <h1>Post não encontrado</h1>;
+    }
+
     return (
         <div>
-            <h1>Visualizando o post {id}</h1>
+            <h1>{post.title}</h1>
+            <p>{post.content}</p>
         </div>
     );
 }
